4fd156c404cb95b09b8639d3924fc9d19beb9050
NAHID <nahidbhuiyan064@gmail.com>
2024-03-15 16:03:49 +0600

    Revert "firebase,fragments"
    This reverts commit b4593a95c571120f2d8184ada5e9636f5cf97869.

--- app/src/main/java/com/mahtabunnabi/cartify/MainActivity.kt
+++ app/src/main/java/com/mahtabunnabi/cartify/MainActivity.kt
@@ -1,11 +1,10 @@
-package com.mahtabunnabi.cartify.views.starter
+package com.mahtabunnabi.cartify
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.mahtabunnabi.cartify.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
